0038475728e2500f8882c5283ab9573226215241
hussein <husseingabriel@outlook.com>
2022-01-06 23:08:28 -0500

    Correccion en carga inicial del script y obtencion del valor username

--- app.js
+++ app.js
@@ -1,38 +1,36 @@
-(function ($) {
-    var inputUser = $('#username')
-    var URL ='https://api.github.com/users/'
-    inputUser.on('keypress', function (e) {
-        if (e.which ===13) {
-            var username =$(this).val.trim().toloverCase()
+$(document).ready(function() {
+    var inputUser = $('#username');
+    var URL = 'https://api.github.com/users/';
+    inputUser.on('keypress', function(e) {
+        if (e.which === 13) {
+            var username = $(this).val().toLowerCase();
             if (username !== "") {
-                var XHR =new XMLHttpRequest()
-                var URLUser =URL + username
-                XHR.open('GET', URLUser)
-                XHR.onreadystatechange = function () {
+                var XHR = new XMLHttpRequest();
+                var URLUser = URL + username;
+                XHR.open('GET', URLUser);
+                XHR.onreadystatechange = function() {
                     if (XHR.readyState === 4) {
-                        var data =JSON.parse(XHR.responseText)
-                        if (data.message && data.message =="Not Found") {
-                            alert("El usuario no existe")
-                        
+                        var data = JSON.parse(XHR.responseText);
+                        if (data.message && data.message == "Not Found") {
+                            alert("El usuario no existe");
+
                         } else {
-                          $('img').attr('src', data.avatar_url)
-                          $('.name').text(data.name)
-                          $('.username').text(data.login)
-                          $('.email').text(data.email)
-                          $('.company').text(data.company)
-                          $('.location').text(data.location)
-                          $('.followers').text(data.followers)
+                            $('img').attr('src', data.avatar_url);
+                            $('.name').text(data.name);
+                            $('.username').text(data.login);
+                            $('.email').text(data.email);
+                            $('.company').text(data.company);
+                            $('.location').text(data.location);
+                            $('.followers').text(data.followers);
                         }
                     }
-                    
-                }
-                XHR.send(null)
+
+                };
+                XHR.send(null);
             }
-           
+
         } else {
-            alert('Debe ingresar un nombre de usuario')
-            
+            //alert('Debe ingresar un nombre de usuario');
         }
-    })
-    
-})
+    });
+});
